af50fa46442596e57ea92cf952a7310b9774e3e3
logan114 <kepirobalazs204@gmail.com>
2024-01-11 11:27:23 +0100

    stadionok

--- adatbeolvasas.py
+++ adatbeolvasas.py
@@ -1,13 +1,33 @@
-nevek_lista = []
-csapatok_lista = []
+from classok import Stadionok
+stadionok_lista = []
 file = open("stadionok.txt","r")
-sorok_lista = file.readlines()[1:]
+sorok = file.readlines()[1:]
 
-def feldolgozas(lista):
-    for i in range(0,len(lista),1):
-        jelen_sor = lista[i]
-        feldolgozott = jelen_sor.split.strip(";")
+def feldolgozas(sorok):
+    for i in range(0,len(sorok),1):
+        jelen_sor = sorok[i]
+        feldolgozott = jelen_sor.strip().split(";")
         nev = feldolgozott[0]
         varos = feldolgozott[1]
-        csapat
-def csapatok_szama(sorok,csapatok):
+        csapat = feldolgozott[2]
+        elso = feldolgozott[3]
+        utolso = feldolgozott[4]
+        stadion = Stadionok(nev,varos,csapat,elso,utolso)
+        stadionok_lista.append(stadion)
+    return stadionok_lista
+
+def csapatok_szama(lista):
+    csapat_ossz:int = int(0)
+    for i in range(0,len(lista),1):
+        csz:int = int(lista[i].csapat)
+        csapat_ossz += csz
+    print (f"A csapatok száma: {csapat_ossz}")
+
+def NewYork(lista):
+    for i in range(0,len(lista),1):
+        if lista[i].varos == "New York":
+            print(f"{lista[i].csapat}, {lista[i].nev}")
+            
+feldolgozas(sorok)
+NewYork(stadionok_lista)
+
--- classok.py
+++ classok.py
@@ -1,4 +1,4 @@
-class stadion:
+class Stadionok:
     def __init__(self, nev, varos,csapat,elso,utolso) -> None:
         self.nev = nev
         self.varos = varos
